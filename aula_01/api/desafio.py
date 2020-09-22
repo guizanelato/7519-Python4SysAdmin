8debc0d0faf7791521010745f1cbee27614cc394
Guilherme Zanelato <guilherme.zanelato@4linux.com.br>
2020-09-21 22:45:07 -0300

    api: adicionado o enunciado do desafio sobre requests

---
+++ aula_01/api/desafio.py
@@ -0,0 +1,16 @@
+"""
+ rot 13 é uma forma clássica de cifrar uma mensagem - consiste em coletar cada caractere
+ de uma mensagem e substituí-lo pelo caractere 13 posições à frente. por exemplo:
+
+     a letra 'a' seria convertida em 'n' 
+     a letra 'b' seria convertida em 'o'
+
+     e assim por diante...
+
+     Considerando o alfabeto que conhecemos e incluindo os algarismos de 0 até 9:
+
+         - faça uma requisição que colete a mensagem cifrada no seguinte endereço: 
+             -> https://python521.herokuapp.com/desafio
+         - utilizando a lógica do rot13, decodifique a mensagem que está no campo 'data' do json recebido 
+
+"""
